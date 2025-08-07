4c02bef15edb82c1122277625da75ff3e3c3a2fd
phumla <phmlotsjhb024@sudent.co.za>
2025-08-07 19:48:30 +0200

    shows the menu, wait for input, handles the choice accordingly

--- login.py
+++ login.py
@@ -1,19 +1,17 @@
 
+print("Please enter an option")
+print("1. Already have an account? Login")
+print("2 .Dont have an account? Register")
+print("3. Exit")
 
-while True:
-    print("Please enter an option")
-    print("1. Already have an account? Login")
-    print("2 .Dont have an account? Register")
-    print("3. Exit")
+choice = input("Enter your choice(1/2/3): ")
 
-    choice = input("Enter your choice(1/2/3): ")
-
-    if choice == "1":
-        print("Please enter login details")
-    elif choice == "2":
-        print("Please register your account")
-    elif choice == "3":
-        print("Exiting Goodbye!")
-        break
-    else:
-        print("Invalid choice")
+if choice == "1":
+    print("Please enter login details")
+elif choice == "2":
+    print("Please register your account")
+elif choice == "3":
+    print("Exiting Goodbye!")
+    
+else:
+    print("Invalid")
